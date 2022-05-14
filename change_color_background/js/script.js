14edaeecdb420b172a27a05b8fa20e8937ef2104
Ravi_Shankar_Nayak <58859919+Ravinayak07@users.noreply.github.com>
2022-05-14 18:58:03 +0530

    update 1 for bg color change: display color name

--- change_color_background/js/script.js
+++ change_color_background/js/script.js
@@ -2,14 +2,18 @@
 const button = document.querySelector('button');
 const body = document.querySelector('body');
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
+const value = document.querySelector('#color_name');
 
 body.style.background = 'red';
 button.addEventListener('click', changeBackground);
 
 function changeBackground(){
     const colorIndex = parseInt(Math.random()*colors.length);
+    value.textContent = colors[colorIndex];
+    console.log(value.textContent);
     body.style.backgroundColor = colors[colorIndex];
 }
+
 
 
 
